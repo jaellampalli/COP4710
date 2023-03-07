8691c074b0684d2fc7304533c3ff4c968d35fbc3
jaellampalli <122390024+jaellampalli@users.noreply.github.com>
2023-03-06 19:34:52 -0500

    Update adduniversity.php

--- php/adduniversity.php
+++ php/adduniversity.php
@@ -31,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $result = mysqli_query($conn, $sql);
         if ($result) {
                  // Account created successfully, redirect to university page
-                 header('Location: adduniversity.php');
+                 header('Location: universities.html');
                }
                else {
                  // Displays error message.
